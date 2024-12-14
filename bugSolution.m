04c66fc29cb7a52e7abafdb8b33bbb9676eada56
BugHunter-X <askzenai@gmail.com>
2024-12-14 18:50:07 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,6 @@
+```matlab
+function result = myFunction(x)
+  % This function calculates the square of a number, correctly handling complex numbers.
+  result = x .^ 2; % Use element-wise power operator
+end
+```
